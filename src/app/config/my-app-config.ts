5742e64f2987423a61866b199a4ee387b84a1453
Spock95 <retkowskimarcin95@gmail.com>
2024-05-06 14:48:11 +0200

    Add my-app-config.ts file for application configuration

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oagwqawqldkQdqNO5d7',
+        issuer: 'https://dev-29561319.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
